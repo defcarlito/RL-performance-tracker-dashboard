956ca8b767b57391c8c7d5f3d0e60c11c663aeb1
defcarlito <harrisoncamorim@gmail.com>
2025-07-15 16:20:38 -0500

    playing w/ layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <div>this is a test</div>
+    <div className="flex flex-wrap h-screen bg-yellow-500">
+      <div className="w-1/2 lg:flex-1 h-1/3 lg:h-full bg-red-500"></div>
+      <div className="w-1/2 lg:flex-1 h-1/3 lg:h-full bg-blue-500"></div>
+      <div className="w-full h-2/3 lg:flex-1 lg:h-full bg-green-500"></div>
     </div>
   )
 }
