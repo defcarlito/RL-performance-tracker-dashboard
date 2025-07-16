d166bdb3ec71a71c9fdaaf84df69a0a8efd1e3e6
defcarlito <harrisoncamorim@gmail.com>
2025-07-16 10:54:25 -0500

    added prettier rc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div className="flex flex-wrap h-screen bg-yellow-500">
-      <div className="w-1/2 lg:flex-1 h-1/3 lg:h-full bg-red-500"></div>
-      <div className="w-1/2 lg:flex-1 h-1/3 lg:h-full bg-blue-500"></div>
-      <div className="w-full h-2/3 lg:flex-1 lg:h-full bg-green-500"></div>
+    <div className="flex h-screen flex-wrap">
+      <div className="flex h-1/3 w-full flex-col items-center justify-center border border-red-500 lg:h-full lg:flex-1/3 gap-4">
+        <div>  
+          <h1 className="text-5xl">BrickBoned</h1>
+          <p>Last played # days ago</p>
+        </div>
+        <div>
+          <p>1v1 - (current)</p>
+          <p>2v2 - (current)</p>
+        </div>
+      </div>
+      <div className="h-2/3 w-full lg:h-full lg:flex-2/3 border border-blue-500">
+        <div className="">
+          <h1>below is the table</h1>
+        </div>
+      </div>
     </div>
   )
 }
