2989e1459a806fcd9884f7ff399f43ad50c24d4a
defcarlito <harrisoncamorim@gmail.com>
2025-07-16 11:25:30 -0500

    added place holders for matches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Match from "@/components/other/match/Match"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="flex h-screen flex-wrap">
-      <div className="flex h-1/3 w-full flex-col items-center justify-center gap-4 border border-red-500 lg:h-full lg:flex-1/3">
+      <div className="flex h-1/3 w-full flex-col items-center justify-center gap-4 border border-red-500 xl:h-full xl:flex-1/3">
         <div>
           <h1 className="text-5xl">BrickBoned</h1>
           <p>Last played # days ago</p>
@@ -13,26 +14,20 @@ export default function Home() {
           <p>2v2 - (current)</p>
         </div>
       </div>
-      <div className="flex h-2/3 w-full flex-col border border-blue-500 lg:h-full lg:flex-2/3 lg:flex-row">
-        <div className="flex-1 overflow-scroll">
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
-          <div>match</div>
+      <div className="flex h-2/3 w-full flex-col border border-blue-500 xl:h-full xl:flex-2/3 xl:flex-row">
+        <div className="flex-1 overflow-scroll flex flex-col gap-2">
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
+            <Match />
         </div>
-        <div className="flex-1 bg-gray-200">
+        <div className="flex-1">
           <div>these are stats about the match</div>
         </div>
       </div>
---
+++ src/components/other/match/Match.tsx
@@ -0,0 +1,23 @@
+export default function Match() {
+  return (
+    <div className="bg-card flex flex-col p-2">
+      <div className="flex flex-wrap justify-between">
+        <div className="flex gap-2">
+          <div>W</div>
+          <div>3-2</div>
+        </div>
+        <div>vs. Brickboned & BrickBone's TM8</div>
+        <div>6/20</div>
+      </div>
+      <div className="flex flex-wrap justify-between">
+        <div className="flex gap-4">
+          <div>+9 MMR</div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div>Match Info</div>
+      </div>
+    </div>
+  )
+}
