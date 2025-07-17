88a135a9441c9d50d87d2fd347be14beee046372
defcarlito <harrisoncamorim@gmail.com>
2025-07-17 14:53:38 -0500

    init fb config

--- src/components/content/match/MatchDashboard.tsx
+++ src/components/content/match/MatchDashboard.tsx
@@ -1,6 +1,8 @@
 import Log from "./log/MatchLog";
 
 export default function Dashboard() {
+
+  
   return (
     <div className="flex h-2/3 w-full flex-col border border-blue-500 xl:h-full xl:flex-2/3 xl:flex-row">
       <div className="flex-1 overflow-scroll">
