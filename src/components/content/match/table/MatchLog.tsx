f109952382aefe4c736a8175617eab91c264cde6
defcarlito <harrisoncamorim@gmail.com>
2025-07-19 13:50:55 -0500

    added some data to cards, needs styling

--- src/components/content/match/table/MatchLog.tsx
+++ src/components/content/match/table/MatchLog.tsx
@@ -22,22 +22,47 @@ type matchProps = {
 
 export function Match({ matchData }: matchProps) {
   const players: Array<Player> = matchData.MatchPlayerInfo
-  const localPlayer: Player | undefined = players.find(playerInfo => playerInfo.EpicAccountId === LOCAL_PLAYER_ID)
+  const localPlayer: Player = players.find(
+    (playerInfo) => playerInfo.EpicAccountId === LOCAL_PLAYER_ID,
+  )!
+
+  const localTeam: number = localPlayer?.Team
+
+  const localTeamScore = localTeam === 0 ? matchData.Team0Score : matchData.Team1Score
+  const opponentTeamScore = localTeam === 0 ? matchData.Team1Score : matchData.Team0Score
+
+  const score: string = `${localTeamScore} - ${opponentTeamScore}`
+
+  const hasLocalPlayerWon = (): boolean => {
+    if (matchData.LocalMMRAfter > matchData.LocalMMRBefore){
+      return true
+    } else if (matchData.LocalMMRAfter < matchData.LocalMMRBefore) {
+      return false
+    }
+    return localTeamScore > opponentTeamScore
+  }
+
+  const cardAccentColor = hasLocalPlayerWon() ? "green" : "red"
+  const cardGradient = `from-${cardAccentColor}-400/50`
+  
+  const mmrDifference = matchData.LocalMMRAfter - matchData.LocalMMRBefore
+  const playlist = matchData.Playlist === 10 ? "1v1" : "2v2"
 
   return (
-    <div className="to-card border-border flex flex-col rounded-2xl border-2 bg-gradient-to-r from-green-400/50 to-15% p-2">
+    <div className={`to-card border-border flex flex-col rounded-2xl border-2 bg-gradient-to-r ${cardGradient} to-15% p-2`}>
       <div className="flex flex-wrap items-center justify-between">
         <div className="flex items-center gap-2">
-          <div className="text-2xl">W</div>
-          <div>3-2</div>
+          <div className="text-2xl">
+            {hasLocalPlayerWon() ? "W" : "L"}
+          </div>
+          <div>{score}</div>
         </div>
-        <div>{localPlayer ? localPlayer.Name : "Unknown"}</div>
+        <div>{localPlayer.Name}</div>
         <div>{matchData.StartDate}</div>
       </div>
-      <div className="align-start flex flex-wrap">
-        <div className="flex gap-4 text-sm">
-          <div>+9 MMR</div>
-        </div>
+      <div className="align-start flex flex-wrap justify-between">
+        <div>(mmr diff)</div>
+        <div>{playlist}</div>
       </div>
     </div>
   )
