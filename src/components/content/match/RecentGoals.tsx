f3eb3c6f1040cb6a5f1104333027364eddc78ab4
harrisonamorim <harrisoncamorim@gmail.com>
2025-10-29 21:38:32 -0500

    component for recent goals

---
+++ src/components/content/match/RecentGoals.tsx
@@ -0,0 +1,16 @@
+export default function RecentGoals() {
+  
+  return (
+    <div
+      id="recent-goals"
+      className="grid max-h-full grid-cols-2 gap-4 overflow-y-scroll"
+    >
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+      <div className="h-[8vw] min-h-16 w-[10vw] min-w-20 bg-red-500">test</div>
+    </div>
+  )
+}
